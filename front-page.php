e41cfb0d82ef34a4288c402c40be336e07f8cb44
Vince <vince@pixelvinestudio.com>
2015-02-26 16:29:39 -0500

    Finish up bulk of home page

--- front-page.php
+++ front-page.php
@@ -1,43 +1,77 @@
+
+<h1 class="aligncenter home-tagline">Complete IT lifecycle solutions</h1>
 <div class="row home-hero">
 	<div class="col-sm-4 col col-1">
-		<div class="block">
+		<div class="block" style="background-image:url(/wp-content/uploads/demo1.jpg);">
 			<figure class="service">
 				<figcaption>
 					<h2>Calibration</h2>
-					<p>Instrumental calibration services to industry, test labs, education, public sector (Defense)</p>
+						<p>
+							ISO/IEC 17025:2005 accreditation
+							Cost-effective solutions
+							On-site calibrations
+						</p>
+					<h2 class="zoom">+</h2>
+					<a href="/calibration"></a>
 				</figcaption>			
 			</figure>
-			<a href="#"></a>
+			<!-- <a href="#"></a> -->
 		</div>
 	</div>
 	<div class="col-sm-4 col col-2">
-		<div class="block">
+		<div class="block" style="background-image:url(/wp-content/uploads/demo3.jpeg);">
 			<figure class="service">
 				<figcaption>
 					<h2>New Hardware</h2>
-					<p>Instrumental calibration services to industry, test labs, education, public sector (Defense)</p>
+						<p>
+							ISO/IEC 17025:2005 accreditation
+							Cost-effective solutions
+							On-site calibrations
+						</p>
+					<h2 class="zoom">+</h2>
+					<a href="/calibration"></a>
 				</figcaption>			
 			</figure>
 		<a href="#"></a>
 		</div>
 
-		<div class="block">
-			<h2>Software</h2>
-			<a href="#"></a>
+		<div class="block" style="background-image:url(/wp-content/uploads/demo2.jpeg);">
+			<figure class="service">
+				<figcaption>
+					<h2>Software</h2>
+					<p>Instrumental calibration services to industry, test labs, education, public sector (Defense)</p>
+					<h2 class="zoom">+</h2>
+					<a href="/calibration"></a>
+				</figcaption>			
+			</figure>
 		</div>
 	</div>
 	<div class="col-sm-4 col col-3">
-		<div class="block">
-			<h2>Services</h2>
+		<div class="block" style="background-image:url(/wp-content/uploads/demo4.jpeg);">
+			<figure class="service">
+				<figcaption>
+					<h2>Services</h2>
+					<a href="/calibration">View more</a>
+					<h2 class="zoom">+</h2>
+					<a href="/calibration"></a>
+				</figcaption>			
+			</figure>
 		</div>
-		<div class="block">
-			<h2>Re-Marketing</h2>
+		<div class="block" style="background-image:url(/wp-content/uploads/demo5.jpeg);">
+			<figure class="service">
+				<figcaption>
+					<h2>Re-Marketing</h2>
+					<a href="/calibration"></a>
+				</figcaption>			
+			</figure>
 		</div>
-		<div class="block">
-			<h2>Used Sales</h2>
+		<div class="block" style="background-image:url(/wp-content/uploads/demo3.jpeg);">
+			<figure class="service">
+				<figcaption>
+					<h2>Used Sales</h2>
+					<a href="/calibration"></a>
+				</figcaption>			
+			</figure>
 		</div>
 	</div>
 </div>
-
-
-<h1 class="aligncenter home-tagline">Complete IT lifecycle solutions</h1>
