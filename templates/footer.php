9a7c75972010627d9d5b7e5cc0c09a9dee3e458d
Vince <vince@pixelvinestudio.com>
2015-03-02 14:59:16 -0500

    Fix for Windows

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,3 @@
 <footer class="content-info" role="contentinfo">
-  <div class="container">
     <p class="copyright">© 2014 NovaStar Solutions. All rights reserved.<a href="http://www.pixelvinecreative.com" target=_blank><img style="margin-left:10px;" src="<?php echo get_bloginfo('template_directory') . "/assets/img/leaf.png"; ?>"></a></p>
-  </div>
 </footer>
--- templates/head.php
+++ templates/head.php
@@ -8,4 +8,7 @@
 	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<?php wp_head(); ?>
+	<!--[if gte IE 8]>
+	<link rel="stylesheet" type="text/css" href="nopath/wp-content/themes/novastar2015/assets/css/ie.css" />
+	<![endif]-->
 </head>
